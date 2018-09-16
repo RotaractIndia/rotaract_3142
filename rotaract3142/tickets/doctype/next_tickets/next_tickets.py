61a71a062e95239f5c6b38238aac728687b45b8e
Neil Lasrado <neil@digithinkit.com>
2018-09-17 00:18:01 +0530

    Next 2018 Ticketing

---
+++ rotaract3142/tickets/doctype/next_tickets/next_tickets.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Neil Lasrado and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class NextTickets(Document):
+	def validate(self):
+		ticket_cost = 200
+		if self.workshop_pass:
+			ticket_cost = 250
+		self.amount = self.qty * ticket_cost
+
+	def on_payment_authorized(self, status_changed_to=None):
+		self.paid = 1
+		self.save(ignore_permissions=True)
