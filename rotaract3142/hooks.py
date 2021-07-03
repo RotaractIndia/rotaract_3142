c696a89d75ae62066dc07cbc70062dbd1c210fad
Neil Trini Lasrado <neil@digithinkit.com>
2021-07-03 20:52:35 +0530

    Multiple Fixes (#43)
    * [feat] Moved AARA to Pranali Project
    
    * [Feat] Moved AARA Nomination Icon to Pranali Project
    
    * [feat] Added FAQs

--- rotaract3142/hooks.py
+++ rotaract3142/hooks.py
@@ -19,6 +19,11 @@ app_license = "MIT"
 # app_include_js = "/assets/rotaract3142/js/rotaract3142.js"
 
 # include js, css files in header of web template
+app_include_js = [
+	"/assets/rotaract3142/js/conf.js"
+	
+]
+
 web_include_css = [
 	"/assets/rotaract3142/css/rotaract.css",
 	"/assets/rotaract3142/css/et-line-font/et-line-font.css",
