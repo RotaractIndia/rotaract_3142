af410e2752c3593e419b21b0869e8ae53d549745
Neil Lasrado <nlasrado@gmail.com>
2021-07-09 00:27:46 +0530

    [Feat] Cleaned Old Website Files

--- rotaract3142/public/js/rotaractblog.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-    $( '.col-xs-12 p:nth-child(2)' ).on( "click", function() {
-      window.open(jQuery(".col-xs-12 p:nth-child(2)").parent(".col-xs-12").find("h1 > a").attr("href"), '_self');
-    });
-});
--- rotaract3142/rotaract_3142/doctype/council_applicant/council_applicant.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Neil Lasrado and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Council Applicant', {
-	// refresh: function(frm) {
-
-	// }
-});
